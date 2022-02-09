2046eb8b4a7d4776405a91ad5077ffd8d228f6de
Darren Kulback <dkulback@gmail.com>
2022-02-08 17:28:22 -0700

    routes namespaced with api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: %i[index show] do
+        resources :items, only: [:index], controller: :merchant_items
+      end
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: %i[index show]
+    end
+  end
 end
