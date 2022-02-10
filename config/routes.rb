646056ad79099dbf1b54eec300192d8491c8874f
Darren Kulback <dkulback@gmail.com>
2022-02-09 18:39:46 -0700

    add nonREST routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants#find'
       resources :merchants, only: %i[index show] do
         resources :items, only: [:index], controller: :merchant_items
       end
@@ -9,7 +10,11 @@ Rails.application.routes.draw do
   end
   namespace :api do
     namespace :v1 do
-      resources :items, only: %i[index show]
+      get '/items/find_all', to: 'items#find'
+      resources :items do
+        resources :merchant, only: :index, controller: :items_merchant
+      end
     end
   end
+  # Non- RESTful
 end
