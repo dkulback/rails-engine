7d5abc9a5af9606c3f5245319d108d1ee6493267
Darren Kulback <dkulback@gmail.com>
2022-02-10 11:36:57 -0700

    Removed method/tests for find

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -27,15 +27,6 @@ class Api::V1::ItemsController < ApplicationController
     end
   end
 
-  def find
-    if params[:name].blank?
-      render json: { data: { message: 'NAME CANT BE BLANK' } }, status: 404
-    else
-      items = Item.search(params[:name])
-      render json: ItemSerializer.new(items)
-    end
-  end
-
   private
 
   def set_item
