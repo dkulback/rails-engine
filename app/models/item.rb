6159096ee887b3653a260bc0036006e43480142e
Darren Kulback <dkulback@gmail.com>
2022-02-09 18:40:10 -0700

    add search functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  validates_presence_of :name, :unit_price, :description
+
+  def self.search(name)
+    where('name ILIKE ?', "%#{name.downcase}%").order(:name)
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.search(name)
+    where('name ILIKE ?', "%#{name.downcase}%").order(:name).first
+  end
 end
