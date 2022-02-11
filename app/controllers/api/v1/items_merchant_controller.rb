45f38bb9207d530a7437c79d5fc14761aa6b0c7b
Darren Kulback <dkulback@gmail.com>
2022-02-10 17:14:48 -0700

    remove conditional

--- app/controllers/api/v1/items_merchant_controller.rb
+++ app/controllers/api/v1/items_merchant_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsMerchantController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    render json: MerchantSerializer.single_merchant(@item.merchant), status: :ok if @item
+    render json: MerchantSerializer.single_merchant(@item.merchant), status: :ok
   end
 end
